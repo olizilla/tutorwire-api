31763337c9d0c2189bdb945c641f408866c0d79d
Alan Shaw <alan@freestyle-developments.co.uk>
2013-05-05 09:33:13 +0100

    Test for getNear

--- test/lib/tutor.js
+++ test/lib/tutor.js
@@ -4,16 +4,36 @@ var async = require('async');
 
 module.exports = {
 	
-	setUp: function(callback) {
-		db.collection('tutors', function(err, tutors) {
-			tutors.drop();
-			callback();
-		});
+	setUp: function(cb) {
+		
+		function onDbOpen(err) {
+			if(err) return console.error('Failed to open database', err);
+			
+			db.collection('tutors', function(err, tutors) {
+				// Ensure the tutors collection is empty
+				tutors.remove({}, cb);
+			});
+		}
+		
+		// If the DB is connected then perform setup straight away...
+		if(db.serverConfig.isConnected()) {
+			onDbOpen();
+		// If the DB is connecting then perform the setup when open
+		} else if(db._state == 'connecting') {
+			db.once('open', onDbOpen);
+		// If the DB is not open or opening then open and perform setup
+		} else {
+			db.open(onDbOpen);
+		}
 	},
 	
-	tearDown: function(callback) {
+	tearDown: function(cb) {
+		
 		// clean up
-		callback();
+		db.close(function(err) {
+			if(err) return console.error('Failed to open database', err);
+			cb();
+		});
 	},
 	
 	'Test getOne validates PUID correctly': function(test) {
@@ -49,6 +69,66 @@ module.exports = {
 			});
 			
 			test.done();
+		});
+	},
+	
+	'Test getNear returns results in expected order': function(test) {
+		
+		// Insert some data
+		var data = [{
+				"name": "David",
+				"subjects": ["Music", "Drama", "Maths"],
+				"location": {
+					"name": "Peckham",
+					"coords": {"lat": 51.473938, "lng": -0.06875}
+				}
+			}, {
+				"name": "Charlie",
+				"subjects": ["Maths", "Physics"],
+				"location": {
+					"name": "Croydon",
+					"coords": {"lat": 51.374667, "lng": -0.097504}
+				}
+			}, {
+				"name": "Inigo",
+				"subjects": ["Sword Fighting"],
+				"location": {
+					"name": "Hoxton",
+					"coords": {"lat": 51.530739, "lng": -0.076861}
+				}
+			}
+		];
+		
+		var tasks = data.map(function(d) {
+			return function(cb) {
+				tutorApi.add(d, cb);
+			};
+		});
+		
+		test.expect(6);
+		
+		// Once the data is inserted, the testing can begin!
+		async.parallel(tasks, function(err) {
+			
+			test.ifError(err);
+			
+			// Portsmouth
+			var coord = {lat: 50.790195, lng: -1.07769};
+			
+			// Method under test
+			tutorApi.getNear(coord, function(err, results) {
+				
+				test.ifError(err);
+				
+				test.equal(results.length, 3);
+				
+				// Results should be in the order Charlie, David, Inigo
+				test.equal(results[0].name, 'Charlie');
+				test.equal(results[1].name, 'David');
+				test.equal(results[2].name, 'Inigo');
+				
+				test.done();
+			});
 		});
 	}
 };
